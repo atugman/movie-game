e224117c863d0e1686db828160d7867319c1a911
Andrew TUGMAN <atugman@Andrews-MacBook-Pro.local>
2017-08-31 14:00:33 -0400

    change api url, remove comments

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,10 @@ import Main from './components/Main'
 import './index.css';
 import {Provider} from 'react-redux';
 import store from './store';
-// import ReduxSweetAlert from 'react-redux-sweetalert';
 
 ReactDOM.render(
   <Provider store={store}>
     <Main />
-    {/* <ReduxSweetAlert /> */}
   </Provider>,
   document.getElementById('root')
 );
