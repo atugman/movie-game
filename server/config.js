56d2bb6b9c0f3a651dd64bbd0f2e6920d4602681
atugman <andrew.tugman@gmail.com>
2020-06-09 00:26:12 -0400

    add restored db url

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://atugman:unc123@ds151059.mlab.com:51059/test2';
+                      'mongodb://atugman:unc123@ds231090.mlab.com:31090/movie-game2';
 exports.PORT = process.env.PORT || 8080;
